5fe78c25f051ce8c18973bc7ac0b001f7c1af4a4
FedericaSpini <spini.1588482@studenti.uniroma1.it>
2020-07-16 16:59:46 +0200

    DTW distance seems working

--- code/src/DTWClassifier.py
+++ code/src/DTWClassifier.py
@@ -4,6 +4,7 @@ import sklearn
 import sklearn.model_selection
 import pandas
 import numpy
+from scipy.spatial import distance
 
 import src.Constants
 import random
@@ -25,25 +26,50 @@ class DTWWordClassifier:
         self.series = self.time_series_manager.get_series()
         self.classes = self.time_series_manager.get_classes()
 
-    def getDTWDistance(self, s1, s2):
-        n = len(s1)
-        m = len(s2)
-        matrix = numpy.empty((n+1, m+1))
-        matrix[:] = numpy.nan
+    def get_samples(self, series_name):
+        # print('\n\n\n\n\n')
+        # print(type(self.series[series_name][0]))
+        # print('\n\n\n\n\n')
+        return self.series[series_name]
+
+    def get_DTW_distance(self, s1, s2):
+        """
+        :param s1: the dataframe representing one time series of a handwriting sample
+        :param s2: the dataframe representing one time series of another handwriting sample
+        :return: the DTW distance
+        """
+        n = s1.shape[0]
+        m = s2.shape[0]
+        print(n, ' - ', m)
+        matrix = numpy.empty((n, m))
+        matrix[:] = numpy.inf
         matrix[0][0] = 0
-        for i in range(1, n+1):
-            for j in range(1, m+1):
+        print(matrix)
+        for i in range(1, n):
+            print('---> i VALE ', i)
+            point1 = s1.iloc[i].to_numpy()
+            for j in range(1, m):
+                print('---> j VALE ', j)
+                point2 = s2.iloc[j].to_numpy()
                 if (i == 0) and (j > 0):
-                    matrix[i][j] = abs(s1[i]-s2[j]) + matrix[i][j-1]
+                    matrix[i][j] = distance.euclidean(point1, point2) + matrix[i][j-1]
                 elif (i > 0) and (j == 0):
-                    matrix[i][j] = abs(s1[i] - s2[j]) + matrix[i-1][j]
+                    matrix[i][j] = distance.euclidean(point1, point2) + matrix[i-1][j]
                 else:
-                    matrix[i][j] = abs(s1[i] - s2[j]) + min(matrix[i - 1][j - 1],
+                    matrix[i][j] = distance.euclidean(point1, point2) + min(matrix[i - 1][j - 1],
                                                             min(matrix[i - 1][j],
                                                                 matrix[i][j - 1]))
-        return matrix[n+1][m+1]
+        return matrix[n-1][m-1]
 
 
 if __name__ == '__main__':
     a = DTWWordClassifier(Utils.MINI_DATASET_NAME, Utils.ITALIC)
+    print((a.get_samples('movementPoints')[0].iloc[0].to_numpy()))
+    print(type(a.get_samples('movementPoints')[0].iloc[0].to_numpy()))
+    print(distance.euclidean(a.get_samples('movementPoints')[0].iloc[0].to_numpy(),
+                       a.get_samples('movementPoints')[5].iloc[0].to_numpy()))
+    print("\n\n\n")
+    print(a.get_DTW_distance(a.get_samples('movementPoints')[35],
+                             a.get_samples('movementPoints')[34]))
+    # print((a.get_samples('movementPoints')[1][0]))
     # print(a)
